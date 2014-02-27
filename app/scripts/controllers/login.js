779ead49722eec82e4422fac977fb7b726dbc311
Hugh Ybarra <hugh.ybarra@gmail.com>
2014-02-27 13:19:17 -0500

    thrusday class start

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -4,19 +4,20 @@ angular.module('mddtestSiteApp')
 .controller('LoginCtrl', ['$scope', '$rootScope','$location','checklogin',function ($scope, $rootScope, $location, checklogin) {
 	//clicks
 	$scope.attemptLogin = function(){
-		$rootScope.loginObj.$login('facebook');
-		var user = $rootScope.loginObj.user.username;
 
-		checklogin.setUser(user);
+		$rootScope.loginObj.$login('facebook').then(function(){
 
-		var userStat =checklogin.isLoggedIn();
+			var user = $rootScope.loginObj.user.username;
 
+			checklogin.setUser(user);
 
-		if (userStat){
-			var view = '/home';
-			$location.path(view);
-		}
+			var userStat =checklogin.isLoggedIn();
 
 
+			if (userStat){
+				var view = '/home';
+				$location.path(view);
+			}
+		});
 	};
 }]);
